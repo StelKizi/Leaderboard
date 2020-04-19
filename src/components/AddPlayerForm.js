f1c3f29440356b56c8dd5615d5a0bc1514a96799
StelKizi <stelkiziridou@gmail.com>
2020-04-19 21:01:25 +0200

    Added stopwatch and crown

---
+++ src/components/AddPlayerForm.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function AddPlayerForm ({addPlayer}) {
+  let playerInput = React.createRef();	
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    addPlayer(playerInput.current.value);
+    e.currentTarget.reset();
+  }
+    
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          ref={playerInput}
+          placeholder="Enter a player's name"
+        />
+
+        <input
+          type="submit"
+          value="Add Player"
+        />
+      </form>
+    )
+}  
